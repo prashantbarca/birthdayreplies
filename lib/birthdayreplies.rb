c2f7135b2422f6135e540d680ad6756798366ae6
Prashant Anantharaman <prashant.barca@gmail.com>
2016-09-12 17:55:13 -0700

    Add methods

--- lib/birthdayreplies.rb
+++ lib/birthdayreplies.rb
@@ -7,7 +7,15 @@ module Birthdayreplies
     attr_accessor :auth_token, :keywords
     # Contructor
     def initialize(options={})
-      puts Validator.validate_options(options)
+      unless Validator.validate_options(options)
+        puts "Try again with options correctly."
+        exit
+      end
+      @graph = Koala::Facebook::API.new(options[:auth_token])
+      posts = @graph.get_connection('me', 'feed', :limit => 100)
+      posts.each do |post|
+        puts p['message']
+      end
     end
   end # End class
 end   # End module
